ef219661fc505f9fffff3aedb6937279c5cdd8f8
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-12 00:21:36 -0300

    nome completo

---
+++ tipos de dados/tiposdedados.js
@@ -0,0 +1,28 @@
+var nome= "Vinicius";
+var idade = 21; 
+
+var time;
+var sobrenome = "Farias"
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto)
+
+var ano = '2021'
+var mes = 3
+console.log(ano + '/' + mes)
+
+var nome1= 'Robson'
+
+var stringnumber = "1tega"
+
+var age = 21;
+
+var nomeproprio = "Vinicius"
+
+var sobrenome = "Silva"
+
+var completename = nomeproprio + sobrenome
+console.log(completename)
+
+var étempo = "It's time"
+
+console.log(typeof completename)
