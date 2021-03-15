afe01714307efd6c3afdd596ecca3e564f557b93
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-15 20:22:42 -0300

    operadores

--- condicionais/script.js
+++ condicionais/script.js
@@ -2,4 +2,47 @@ var possuiGraduacao = true;
 
 if(possuiGraduacao){
   console.log('Possui Graduacao')
+}else{
+  console.log('É falso')
 }
+
+//falso
+
+// if(false){
+//   if(0)
+// }
+
+// if(NaN)
+// if(null)
+// 0 é sempre falso
+if(( 5 -5 ) && (5 + 5)){
+  console.log('Verdadeiro');
+}else{
+  console.log('Falso')
+}
+
+var corFavorita = "azul"
+
+switch(corFavorita){
+  case 'azul':
+  console.log("Olhe para o céu")
+  break;
+  case 'verde':
+    console.log("Olhe para a floresta")
+    break;
+
+  default:
+    console.log("Olhe para o sol")
+}
+
+var expressao = (5 -2) && (5 - '') && (5-2);
+console.log(expressao)
+
+var nome= "Vinicius"
+var idade = 20
+var possuiDoutorado = false
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+var brasil = 207
+var china = 1340
