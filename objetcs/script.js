36e58ffff6f85019bb7665b023765ed0e887f4d6
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-15 21:22:23 -0300

    objetos math

--- objetcs/script.js
+++ objetcs/script.js
@@ -3,3 +3,22 @@ var pessoa = {
   idade: 20,
   profissao: 'Freelancer'
 }
+
+var quadrado = {
+  lados: 4,
+  area: function (lado){
+    return lado * lado
+  },
+  perimetro: function (lado){
+    return this.lados *lado;
+  }
+}
+
+console.log(quadrado.area(5))
+console.log(quadrado.perimetro(5))
+
+Math.PI
+Math.random()
+
+var pi = Math.PI
+console.log(Math.random)
