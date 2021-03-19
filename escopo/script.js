770e0de80b1aaacfe01233eda85504fe91e00773
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-19 11:54:08 -0300

    escopo

---
+++ escopo/script.js
@@ -0,0 +1,42 @@
+var carro = 'Fusca'
+
+function mostrarCarro(){
+  console.log(carro)
+}
+
+mostrarCarro()
+console.log(carro)
+
+
+
+for(let i = 0; i<10; i++){
+  console.log(`Numero ${i}`)
+}
+
+const mes = 'Dezembro'
+
+const data = {
+  dia: 28,
+  mes: 'Dezembro',
+  semana: 'Sexta'
+}
+
+data.dia = 25
+
+const dois = 2;
+function somarDois(x){
+  return x + dois;
+}
+function dividirDois(x){
+  return x + dois;
+}
+somarDois(4)
+dividirDois(6)
+const numero = 50
+
+for(let numero = 0; numero < 10; numero++){
+  console.log(numero)
+}
+
+const total = 10 *numero;
+console.log(total)
