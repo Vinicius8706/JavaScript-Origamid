aa0ac845fbfcd2a9b20f92cb50ab2a4b2f0337e0
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-19 13:41:12 -0300

    dom beginners

---
+++ DOM-INICIANTES/script.js
@@ -0,0 +1,12 @@
+//window.alert('Isso mesmo')
+
+const href = window.location.href
+
+console.log(href)
+ if(href === 'http://127.0.0.1:5500/index.html'){
+   console.log('É igual')
+}
+
+document.querySelector('h1')//Seleciona uma tag dentro do html
+document.body
+
