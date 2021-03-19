fd9a45c9bbdcafd960acfe32b7d680cb9107ac6f
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-19 11:33:48 -0300

    escopo

--- condicionais/script.js
+++ condicionais/script.js
@@ -5,3 +5,18 @@ var naoPossuiDiabetes = false;
 podeBeber = (idade >=18 && naoPossuiDiabetes) ? true : false;
 
 console.log(podeBeber)
+
+var possuiFaculdade = true;
+if(possuiFaculdade){
+  console.log('Sim possui')
+}else{
+  console.log('Nao possui')
+}
+
+var scroll = 1000;
+scroll =+ scroll
+
+var darCredito = (possuiCarro && possuiCasa) ;
+var possuiCarro = true;
+var possuiCasa = true;
+console.log(darCredito)
