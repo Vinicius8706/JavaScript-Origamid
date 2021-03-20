13fc3cae1062de29756c5cb3eee300332ec57ad2
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-20 19:48:16 -0300

    javascript begin

--- selecionando-o-dom/script.js
+++ selecionando-o-dom/script.js
@@ -4,3 +4,15 @@ console.log(animais.innerText)
 const gridSection = document.getElementsByClassName('grid-section');
 
 console.log(gridSection[1])
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl)
+
+const linkInterno = document.querySelector('[href^="#"]')
+console.log(linkInterno)
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg[1])
