4db167ddd706537bef285c6682da0273f87572e3
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-20 19:42:43 -0300

    javascript begin

---
+++ selecionando-o-dom/script.js
@@ -0,0 +1,6 @@
+const animais = document.getElementById('animais')
+console.log(animais.innerText)
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+console.log(gridSection[1])
