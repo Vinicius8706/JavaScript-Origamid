f87a212c2d1611b5cbf51f86aaa51ae31ee9d82c
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-20 18:12:44 -0300

    dom

--- DOM-INICIANTES/script.js
+++ DOM-INICIANTES/script.js
@@ -7,6 +7,27 @@ console.log(href)
    console.log('É igual')
 }
 
-document.querySelector('h1')//Seleciona uma tag dentro do html
+const h1Selecionado = document.querySelector('h1') //Seleciona uma tag dentro do html
+
+console.log(h1Selecionado)
 document.body
 
+// const h1Classes = h1selecionado.classList
+
+function callbackh1(){
+  console.log('Clicou em ', h1Selecionado.innerText )
+}
+
+h1Selecionado.addEventListener('click', callbackh1)
+
+const hrefPagina = window.location.href;
+console.log(hrefPagina)
+
+const elementoAtivo = document.querySelector('.ativo')
+
+const linguagem = window.navigator.language
+console.log(linguagem)
+
+const windowwidth = window.innerWidth;
+
+console.log(windowwidth)
