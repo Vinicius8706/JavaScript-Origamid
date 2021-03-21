943760174cf1b728d854b2c4c6206d04b7ae8fc7
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-21 11:02:23 -0300

    script aula dom

--- selecionando-o-dom/script.js
+++ selecionando-o-dom/script.js
@@ -34,3 +34,23 @@ const arrayGrid = Array.from(gridSectionHTML)
 arrayGrid.forEach(function(item){
   console.log(item)
 })
+
+const animaisClass = document.querySelectorAll('img')
+
+console.log(animaisClass)
+
+const imagem = document.querySelectorAll('img[src^="img/imagem"]')
+
+console.log(imagem)
+
+const href = document.querySelectorAll('[href^="#"]')
+
+console.log(href)
+
+const h2Animais = document.querySelector('animais-descricao h2')
+
+console.log(h2Animais)
+
+const paragrafos = document.querySelectorAll('p')
+
+console.log(paragrafos[paragrafos.length - 1])
