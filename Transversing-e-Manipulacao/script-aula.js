51fec59d3a13a45eebfacffc145d2413aa25cb1a
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-22 15:44:32 -0300

    innerhtml

---
+++ Transversing-e-Manipulacao/script-aula.js
@@ -0,0 +1,39 @@
+// const h1 = document.querySelector('h1');
+// const animaisLista = document.querySelector('.animais-lista');
+
+// h1.innerHTML;
+// console.log(animaisLista.innerHTML)
+
+const lista = document.querySelector('.animais-lista')
+
+console.log(lista.children[lista.children.length-1]) // ultimo filho
+
+console.log(
+  lista.querySelector('li:last-child'))
+
+console.log(lista.previousSibling)
+
+const animais = document.querySelector('.animais')
+const contato = document.querySelector('.contato')
+const titulo = contato.querySelector('.titulo')
+const mapa = document.querySelector('.mapa')
+
+contato.replaceChild(mapa, titulo)
+
+// contato.insertBefore(animais, mapa)
+
+// const novoh1 = document.createElement('h1')
+// novoh1.innerHTML ='Novo Título'
+// novoh1.classList.add('titulo')
+
+// mapa.appendChild(novoh1)
+
+// console.log(novoh1)
+
+
+const h1 = document.querySelector('h1');
+const faq = document.querySelector('.faq')
+
+const cloneh1 = h1.cloneNode(true)
+cloneh1.classList.add('azul')
+faq.appendChild(cloneh1)
--- Transversing-e-Manipulacao/script.js
+++ Transversing-e-Manipulacao/script.js
@@ -1,31 +1,16 @@
-const h1 = document.querySelector('h1');
-const animaisLista = document.querySelector('.animais-lista');
+const menu = document.querySelector('.menu')
+const copy = document.querySelector('.copy')
 
-h1.innerHTML;
-console.log(animaisLista.innerHTML)
+const cloneMenu = menu.cloneNode(true)
 
-const lista = document.querySelector('.animais-lista')
+copy.appendChild(cloneMenu)
 
-console.log(lista.children[lista.children.length-1]) // ultimo filho
+const faq = document.querySelector('.faq')
 
-console.log(
-  lista.querySelector('li:last-child'))
-
-console.log(lista.previousSibling)
+const firstChild = faq.querySelector('dt')
+const primeiroDD = firstChild.nextElementSibling;
+console.log(primeiroDD)
 
 const animais = document.querySelector('.animais')
-const contato = document.querySelector('.contato')
-const titulo = contato.querySelector('.titulo')
-const mapa = document.querySelector('.mapa')
 
-contato.replaceChild(mapa, titulo)
-
-// contato.insertBefore(animais, mapa)
-
-const novoh1 = document.createElement('h1')
-novoh1.innerHTML ='Novo Título'
-novoh1.classList.add('titulo')
-
-mapa.appendChild(novoh1)
-
-console.log(novoh1)
+faq.innerHTML = animais.innerHTML
