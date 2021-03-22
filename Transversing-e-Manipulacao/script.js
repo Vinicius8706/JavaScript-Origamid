9c1bd16d844966eb64a2d24d6b2bb7289ee880f7
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-22 14:24:35 -0300

    clonar elemento

--- Transversing-e-Manipulacao/script.js
+++ Transversing-e-Manipulacao/script.js
@@ -12,3 +12,20 @@ console.log(
   lista.querySelector('li:last-child'))
 
 console.log(lista.previousSibling)
+
+const animais = document.querySelector('.animais')
+const contato = document.querySelector('.contato')
+const titulo = contato.querySelector('.titulo')
+const mapa = document.querySelector('.mapa')
+
+contato.replaceChild(mapa, titulo)
+
+// contato.insertBefore(animais, mapa)
+
+const novoh1 = document.createElement('h1')
+novoh1.innerHTML ='Novo Título'
+novoh1.classList.add('titulo')
+
+mapa.appendChild(novoh1)
+
+console.log(novoh1)
