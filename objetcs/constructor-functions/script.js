0aa4cd13e61014ae9d3efaae29f8697317afb71b
Vinicius Farias <fsvinicius9@gmail.com>
2021-03-22 21:00:33 -0300

    addClass

--- objetcs/constructor-functions/script.js
+++ objetcs/constructor-functions/script.js
@@ -45,6 +45,11 @@ function Dom(seletor){
       element.classList.add(classe)
     })
   }
+  this.removeClass = function(classe){
+    elementList.forEach((element)=>{
+    element.classList.remove(classe)
+    })
+  }
 }
 
 const listaItens = new Dom('li')
