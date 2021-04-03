5c4fd7812ac4676a9d49e69855a03183a525b264
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-03 00:32:45 -0300

    consertando js

--- efeitos-no-dom/dropdown/js/modules/outside-click.js
+++ efeitos-no-dom/dropdown/js/modules/outside-click.js
@@ -3,21 +3,21 @@ export default function outsideClick(element, events ,callback) {
   const html = document.documentElement
   const outside = 'data-outside'
 
-  if(element.hasAttribute(outside)){
+  if(!element.hasAttribute(outside)){
       events.forEach(userEvent =>{
-      html.addEventListener('click',handleOutsideClick)
-
+        html.addEventListener(userEvent,handleOutsideClick)
+      
     })
+    
     element.setAttribute(outside, '')
-
   }
   function handleOutsideClick(event) {
     if(!element.contains(event.target)){
       element.removeAttribute(outside)
       html.removeEventListener('click',handleOutsideClick)
       events.forEach(userEvent =>{
-        html.removeEventListener('click',handleOutsideClick)
-  
+        html.removeEventListener(userEvent,handleOutsideClick)
+        
       })
       callback()
     }
