7e21723d4d5be3cb5db62d64bdd13b971c44eba9
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-07 14:47:36 -0300

    api

---
+++ api-http/script.js
@@ -0,0 +1,8 @@
+const url = 'https//jsonplaceholder.typicode.com/posts/'
+const options = {
+  method: 'POST',
+  body: '{"title: "JavaScript"}',
+  headers: {
+    "Content-Type": "application/json; charset=utf-8"
+  }
+}
