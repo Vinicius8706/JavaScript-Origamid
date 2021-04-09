b8dba5299193660b1a9e9fa7e017c430eb4edd30
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-09 19:33:17 -0300

    fetch animais

---
+++ efeitos-no-dom/animais-fetch/js/script.js
@@ -0,0 +1,21 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initToolTip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+import initmenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+import initFetchAnimais from './modules/fetch-animais'
+
+initScrollSuave();
+initAnimacaoScroll()
+initAccordion();
+initTabNav()
+initModal()
+initToolTip();
+initDropdownMenu()
+initmenuMobile()
+initAnimaNumeros()
+initFetchAnimais()
