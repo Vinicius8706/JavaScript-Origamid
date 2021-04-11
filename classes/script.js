6377882f47293a9fece94c06975b36fe7a47632f
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-11 16:45:38 -0300

    static

---
+++ classes/script.js
@@ -0,0 +1,45 @@
+class Button {
+  constructor(text,background,color){
+    this.text = text;
+    this.background = background;
+    this.color = color;
+  }
+  element(){
+    const buttonElement = document.createElement("button")
+    buttonElement.innerText = this.text;
+    buttonElement.style.background = this.background
+    buttonElement.style.color = this.color
+    return buttonElement
+  }
+  appendTo(target) {
+    const targetElement = document.querySelector(target)
+    targetElement.appendChild(this.element())
+    return targetElement  }
+    static blueButton(text){
+      return new Button(text,'blue','white')
+    }
+}
+
+const botaoAzul = Button.blueButton('Comprar')
+
+// class Button {
+//   cronstructor(options) {
+//     this.options = options
+//   }
+//   static createButton(text, background){
+//     const buttonElement = document.createElement('button')
+//     buttonElement.innerText = text
+//     buttonElement.background = background
+//     return buttonElement
+//   }
+// }
+
+// const blueButton = new Button({
+//   backgroundColor: 'blue',
+//   text: 'Comprar',
+//   color:'white'
+// })
+
+// const blueButtonStatic = Button.createButton('Clique', 'blue')
+
+// console.log(blueButtonStatic)
