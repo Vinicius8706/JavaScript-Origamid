90acde421f1e220c6ce3c112ae899b636f96436e
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-28 14:21:55 -0300

    webpack

--- efeitos-no-dom/animais-fetch/js/script.js
+++ efeitos-no-dom/animais-fetch/js/script.js
@@ -1,23 +1,23 @@
-import initScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
-import initAccordion from './modules/accordion.js'
-import initTabNav from './modules/tabnav.js'
-import initModal from './modules/modal.js'
-import initToolTip from './modules/tooltip.js'
-import initDropdownMenu from './modules/dropdown-menu.js'
-import initmenuMobile from './modules/menu-mobile.js'
-import initFuncionamento from './modules/funcionamentos.js'
-import initFetchAnimais from './modules/fetch-animais.js'
-import initFetchBitcoin from './modules/fetch-bitcoin.js'
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initmenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamentos.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
-initAnimacaoScroll()
+initAnimacaoScroll();
 initAccordion();
-initTabNav()
-initModal()
+initTabNav();
+initModal();
 initToolTip();
-initDropdownMenu()
-initmenuMobile()
-initFuncionamento() 
-initFetchAnimais()
-initFetchBitcoin()
+initDropdownMenu();
+initmenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
